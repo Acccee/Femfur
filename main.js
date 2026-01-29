8a9428837974245aca4fe07c29d15120e8a0aab5
Sesla <117109675+Seslass@users.noreply.github.com>
2026-01-29 22:30:46 +0200

    Update Supabase anon key in main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // SUPABASE CONFIGURATION
 const SUPABASE_URL = 'https://httzzyjltrfzagkipojv.supabase.co';
-const SUPABASE_ANON_KEY = 'httzzyjltrfzagkipojv';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0dHp6eWpsdHJmemFna2lwb2p2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3MTY0NTksImV4cCI6MjA4NTI5MjQ1OX0.o210K6q3HRNbRsDgTEYlzMYGUm1Gk0cgYWMj4zGe3j0';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
